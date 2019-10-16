f9f56d28639aca9ce1f87bfb29480c0b3b4e31f2
Bora Manga <bora.mng525@gmail.com>
2019-10-17 00:01:27 +0200

    react-redux-form revisited

--- src/components/DishdetailComponent task1tamam.js
+++ src/components/DishdetailComponent task1tamam.js
@@ -11,6 +11,7 @@ import {
   Modal
 } from "reactstrap";
 import { Link } from "react-router-dom";
+import { Loading } from "./LoadingComponent";
 
 class CommentForm extends Component {
   constructor(props) {
@@ -80,28 +81,45 @@ function RenderComments({ comments }) {
 }
 
 const DishDetail = props => {
-  if (props.dish != null)
+  if (props.isLoading) {
     return (
       <div className="container">
         <div className="row">
-          <Breadcrumb>
-            <BreadcrumbItem>
-              <Link to="/menu">Menu</Link>
-            </BreadcrumbItem>
-            <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
-          </Breadcrumb>
-          <div className="col-12">
-            <h3>{props.dish.name}</h3>
-            <hr />
-          </div>
-        </div>
-        <div className="row">
-          <RenderDish dish={props.dish} />
-          <RenderComments comments={props.comments} />
+          <Loading />
         </div>
       </div>
     );
-  else return <div></div>;
+  } else if (props.errMess) {
+    return (
+      <div className="container">
+        <div className="row">
+          <h4>{props.errMess}</h4>
+        </div>
+      </div>
+    );
+  } else if (props.dish != null)
+    if (props.dish != null)
+      return (
+        <div className="container">
+          <div className="row">
+            <Breadcrumb>
+              <BreadcrumbItem>
+                <Link to="/menu">Menu</Link>
+              </BreadcrumbItem>
+              <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+            </Breadcrumb>
+            <div className="col-12">
+              <h3>{props.dish.name}</h3>
+              <hr />
+            </div>
+          </div>
+          <div className="row">
+            <RenderDish dish={props.dish} />
+            <RenderComments comments={props.comments} />
+          </div>
+        </div>
+      );
+    else return <div></div>;
 };
 
 export default DishDetail;
